12c02b012b7d2b4856f691f6d4b0cf0dccce8fd2
saibadarinadh <vechasaibadarinadh@gmail.com>
2024-12-29 18:13:00 +0530

    small ui change

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -1,9 +1,8 @@
 // components/loading.js
 export default function Loading() {
-    return (
-      <div className="flex items-center justify-center h-32">
-        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid border-opacity-50"></div>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="flex items-center justify-center h-32">
+      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid border-opacity-50"></div>
+    </div>
+  );
+}
