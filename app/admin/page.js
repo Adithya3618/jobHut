b5bd5561490f6886b92b4e14960d7948d979dbfc
saibadarinadh <vechasaibadarinadh@gmail.com>
2025-01-04 11:19:07 +0530

    added Google Tag Manager

--- app/admin/page.js
+++ app/admin/page.js
@@ -4,25 +4,17 @@ import { Suspense } from 'react'
 import AdminLogin from '../components/AdminLogin'
 import Loading from '../components/Loading'
 import { usePageViews } from '../hooks/usePageViews'
-import Footer from '../components/Footer'
-import Header from '../components/Header'
-
-export const dynamic = 'force-dynamic'
 
 export default function Admin() {
-  usePageViews()
+  usePageViews();
 
   return (
-    <div className="min-h-screen flex flex-col bg-gray-50">
-      <Header />
-      <main className="flex-grow">
-        <Suspense fallback={<Loading />}>
-          <div className="container mx-auto px-4 py-8">
-            <AdminLogin />
-          </div>
-        </Suspense>
-      </main>
-      <Footer />
+    <div>
+      <h1>Admin Login</h1>
+      <Suspense fallback={<Loading />}>
+        <AdminLogin />
+      </Suspense>
     </div>
   )
 }
+
