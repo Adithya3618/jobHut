94b977967dd791710d965f3db296e3dea85b4d2b
saibadarinadh <vechasaibadarinadh@gmail.com>
2025-01-04 11:17:17 +0530

    added Google Tag Manager

--- app/admin/page.js
+++ app/admin/page.js
@@ -7,22 +7,22 @@ import { usePageViews } from '../hooks/usePageViews'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 
-
-
-
-      
+export const dynamic = 'force-dynamic'
 
 export default function Admin() {
-  usePageViews();
+  usePageViews()
 
   return (
-    <div>
+    <div className="min-h-screen flex flex-col bg-gray-50">
       <Header />
-      <Suspense fallback={<Loading />}>
-        <AdminLogin />
-      </Suspense>
-      <Footer/>
+      <main className="flex-grow">
+        <Suspense fallback={<Loading />}>
+          <div className="container mx-auto px-4 py-8">
+            <AdminLogin />
+          </div>
+        </Suspense>
+      </main>
+      <Footer />
     </div>
   )
 }
-
